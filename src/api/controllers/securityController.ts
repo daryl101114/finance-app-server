253b8c68219628e8cc2d1f0239e569ad544a6519
Daryl Santillan <johns@getchoice.com>
2023-10-26 18:48:18 -0500

    refactored code to implement controllers

--- src/api/controllers/securityController.ts
+++ src/api/controllers/securityController.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express";
 import mongoose from "mongoose";
+import { User } from "../../model/user";
+
+interface SecurityResponse {
+  message: string;
+}
+
+// interface ISecurityController extends SecurityController
 
 class SecurityController {
-  public async createUser(req: Request, res: Response): Promise<void> {
+  public static async createUser(req: Request): Promise<SecurityResponse> {
     //Handle Create User Here
+    try {
+      const { firstName, lastName, email, username, password } = req.body;
+
+      const user = User.build({
+        firstName,
+        lastName,
+        email,
+        username,
+        password,
+      });
+      await user.save();
+      return { message: "User created successfuly" };
+    } catch (err) {
+      console.error(err);
+      return { message: "Failed to create user" };
+    }
   }
 }
 
-export default SecurityController;
+export { SecurityController };
--- src/api/route/securityRoutes.ts
+++ src/api/route/securityRoutes.ts
@@ -1,14 +1,10 @@
 import express, { Request, Response } from "express";
-import { User } from "../../model/user";
-
+import { SecurityController } from "../controllers/securityController";
 const router = express.Router();
 
 router.post("/api/registerUser", async (req: Request, res: Response) => {
-  const { firstName, lastName, email, username, password } = req.body;
-
-  const user = User.build({ firstName, lastName, email, username, password });
-  await user.save();
-  return res.status(201).send(user);
+  const response = SecurityController.createUser(req);
+  return res.status(201).send(response);
 });
 
 export { router as userRouter };
