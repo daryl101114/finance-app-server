89128e7fc5345170040c193cc16614d76a290788
Daryl Santillan <johns@getchoice.com>
2023-10-26 23:53:14 -0500

    uncomment out codes

--- src/database/dbConnect.ts
+++ src/database/dbConnect.ts
@@ -2,17 +2,17 @@ import dotenv from "dotenv";
 const mongoose = require("mongoose");
 
 const mongooseConnect = (MONGO_URI: string): void => {
-  // mongoose
-  //   .connect(MONGO_URI)
-  //   .then(() => {
-  //     console.log("connected to database successfully...");
-  //   })
-  //   .catch((error: Object) => {
-  //     console.log(
-  //       "failed to connect to the database. terminating the application..."
-  //     );
-  //     console.error(error);
-  //     process.exit(1);
-  //   });
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      console.log("connected to database successfully...");
+    })
+    .catch((error: Object) => {
+      console.log(
+        "failed to connect to the database. terminating the application..."
+      );
+      console.error(error);
+      process.exit(1);
+    });
 };
 export { mongooseConnect };
